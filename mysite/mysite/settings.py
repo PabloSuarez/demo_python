ddb0a2f075975ef9e9af2531ae9caa92ef4bff85
psuarez <hostsuarezpablo@gmail.com>
2015-09-14 17:58:52 -0300

    update

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -100,3 +100,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ENGINE = 'django.db.backends.sqlite3'
+NAME = 'db.sqlite3'
+
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
